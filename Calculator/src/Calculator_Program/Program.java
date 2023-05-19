4e3462bbf0d7aa0360d65ef652c12979232e2037
kye <kye6735@naver.com>
2023-05-19 09:09:13 +0900

    Calculator Program 서버 및 프로그램 커밋

---
+++ Calculator/src/Calculator_Program/Program.java
@@ -0,0 +1,10 @@
+package Calculator_Program;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Calculator/src/Calculator_Program/Server.java
@@ -0,0 +1,8 @@
+package Calculator_Program;
+
+public class Server {
+	public static void main(String[] args) {
+		new Program();
+		
+	}
+}

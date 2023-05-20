db72f26e18f665de2ce4267c203139c5d2d51f4a
kye <kye6735@naver.com>
2023-05-20 17:25:46 +0900

    함수 지정 시 클릭해야 계산 화면이 나와서 다시 수정

--- Calculator/src/Calculator_Program/Program.java
+++ Calculator/src/Calculator_Program/Program.java
@@ -14,19 +14,14 @@ public class Program extends JFrame {
 	// ArrayList에 숫자와 연산 기호를 하나씩 구분해 넣음
 	
 	public Program() {
-		Calculator();	// 따로 관리하기 위해서 함수로 지정함
-		
+
 		inputSpace = new JTextField();
 		inputSpace.setEditable(false);	// 편집가능여부 : X (버튼만 사용하여야 함)
 		inputSpace.setBackground(Color.WHITE);	// background 색상 설정
 		inputSpace.setHorizontalAlignment(JTextField.RIGHT);	// 정렬위치 설정
 		inputSpace.setFont(new Font("Arial", Font.BOLD, 50));	// font 설정
 		inputSpace.setBounds(8, 10, 450, 100);	// x, y, width, height
-		
-		add(inputSpace);
-	} // Program() 
-	
-	public void Calculator() {
+
 		setTitle("계산기");	// 타이틀 설정
 		setSize(480, 600);	// 프레임 크기 설정
 		setVisible(true);	// 프레임 보이게 설정
@@ -34,7 +29,9 @@ public class Program extends JFrame {
 		setLocationRelativeTo(null);	// 프레임이 센터로 오도록함
 		setLayout(null);	// 레이아웃을 내 마음대로 설정
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// JFrame을 정상적으로 종료
-	} // Calculator()
+
+		add(inputSpace);
+	} // Program() 
 	
 	
 } // JFrame	

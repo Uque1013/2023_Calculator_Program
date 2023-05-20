6bf4498999023f2cdb0a4ff9c95c04d1e24aec3d
kye <kye6735@naver.com>
2023-05-20 18:43:53 +0900

    JPanel 이용해서 연산 및 숫자 버튼 구현

--- Calculator/src/Calculator_Program/Program.java
+++ Calculator/src/Calculator_Program/Program.java
@@ -20,10 +20,37 @@ public class Program extends JFrame {
 		inputSpace.setBackground(Color.WHITE);	// background 색상 설정
 		inputSpace.setHorizontalAlignment(JTextField.RIGHT);	// 정렬위치 설정
 		inputSpace.setFont(new Font("Arial", Font.BOLD, 50));	// font 설정
-		inputSpace.setBounds(8, 10, 450, 100);	// x, y, width, height
+		inputSpace.setBounds(8, 15, 450, 120);	// x, y, width, height
 
+		JPanel btnPanel = new JPanel();
+		btnPanel.setLayout(new GridLayout(4, 4, 10, 15));	// 격자형태 레이아웃 (가로 칸수, 세로 칸수, 좌우 간격, 위아래 간격)
+		btnPanel.setBounds(8, 160, 450, 300);
+		
+		String btn_names[] = {"C", "÷", "=", "×", 
+				"7", "8", "9", "-",
+				"4", "5", "6", "+",
+				"1", "2", "3", "0"};
+		JButton btn[] = new JButton[btn_names.length];	// 버튼들 배열
+		
+		for(int i = 0; i < btn_names.length; i++) {
+			btn[i] = new JButton(btn_names[i]);
+			btn[i].setFont(new Font("Arial", Font.BOLD, 25));	// Font
+			// 버튼 색 지정
+			if(btn_names[i] == "C")
+				btn[i].setBackground(Color.BLUE);
+			else if((i >= 4 && i <= 6) || (i >= 8 && i <= 10) || (i >= 12 && i <= 14))
+				btn[i].setBackground(Color.DARK_GRAY);
+			else 
+				btn[i].setBackground(Color.GRAY);
+			
+			// font 색 지정	
+			btn[i].setForeground(Color.WHITE);
+			btn[i].setBorderPainted(false);	// 테두리 제거
+			btnPanel.add(btn[i]);
+		}
+		
 		setTitle("계산기");	// 타이틀 설정
-		setSize(480, 600);	// 프레임 크기 설정
+		setSize(480, 520);	// 프레임 크기 설정
 		setVisible(true);	// 프레임 보이게 설정
 		setResizable(false);	// 프레임 크기 변경 X
 		setLocationRelativeTo(null);	// 프레임이 센터로 오도록함
@@ -31,7 +58,9 @@ public class Program extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// JFrame을 정상적으로 종료
 
 		add(inputSpace);
+		add(btnPanel);
 	} // Program() 
+	
 	
 	
 } // JFrame	
